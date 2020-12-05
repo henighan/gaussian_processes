e5f834052764878c233b4acccb5edb144043ac97
Tom Henighan <henighan.2@gmail.com>
2020-12-05 10:25:35 -0800

    progress

--- gaussian_processes.py
+++ gaussian_processes.py
@@ -3,13 +3,13 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 
-def radial_basis_fn(x1, x2, sigma=1, l=0.1):
+def radial_basis_fn(x1, x2, sigma=1.0, l=0.03):
     return (sigma ** 2) * np.exp(-((x1 - x2) ** 2) / 2 / l ** 2)
 
 
 def main():
     n_frequencies = 3
-    n_points_to_plot = 50
+    n_points_to_plot = 300
     training_set_size = 10
     assert training_set_size < n_points_to_plot
     min_frequency, max_frequency = 0.5, 3.0
@@ -34,9 +34,23 @@ def main():
     sigma_train_test = sigma[train_inds, :][:, test_inds]
     sigma_test_train = sigma_train_test.transpose()
     predicted_means = sigma[:, train_inds] @ np.linalg.inv(sigma_train_train) @ y_train
-    plt.plot(x_train, y_train, linestyle="", marker="o", label="training data")
-    plt.plot(x, y, label="true")
-    plt.plot(x, predicted_means, label="predicted")
+    predicted_covariance = (
+        sigma
+        - sigma[:, train_inds] @ np.linalg.inv(sigma_train_train) @ sigma[train_inds, :]
+    )
+    predicted_sigmas = np.diag(predicted_covariance)
+    plt.plot(
+        x_train, y_train, linestyle="", marker="o", label="training data", color="r"
+    )
+    plt.plot(x, y, label="true", color="b", linestyle="--")
+    # plt.plot(x, predicted_means, label="predicted")
+    plt.fill_between(
+        x,
+        predicted_means - predicted_sigmas,
+        predicted_means + predicted_sigmas,
+        color="gray",
+        alpha=0.2,
+    )
     plt.legend()
     # plt.imshow(sigma)
     # plt.colorbar()
