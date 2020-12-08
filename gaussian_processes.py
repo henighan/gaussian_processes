eab014aea9da8ee6ccc0b531cfe8a146adfe5112
Tom Henighan <henighan.2@gmail.com>
2020-12-07 21:26:11 -0800

    some refactoring and docstrings

--- gaussian_processes.py
+++ gaussian_processes.py
@@ -4,51 +4,6 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 
-def radial_basis_fn(x1, x2, sigma=1.0, l=0.06):
-    return (sigma ** 2) * np.exp(-((x1 - x2) ** 2) / 2 / l ** 2)
-
-
-def generate_ground_truth_data(
-    n_points: int = 300,
-    n_frequencies: int = 3,
-    frequency_range: Tuple[float, float] = (0.5, 3.0),
-    amplitude_range: Tuple[float, float] = (0.1, 1.0),
-) -> Tuple[np.ndarray, np.ndarray]:
-    xmin, xmax = 0.0, 1.0
-    x = np.linspace(xmin, xmax, num=n_points)
-    y = np.zeros_like(x)
-    for _ in range(n_frequencies):
-        sampled_frequency = random.uniform(*frequency_range)
-        sampled_amplitude = random.uniform(*amplitude_range)
-        y += sampled_amplitude * np.sin(2 * np.pi * sampled_frequency * x)
-    return x, y
-
-
-def generate_plot(
-    x: np.ndarray,
-    y: np.ndarray,
-    train_inds: np.ndarray,
-    predicted_means: np.ndarray,
-    predicted_variances: np.ndarray,
-):
-    x_train, y_train = x[train_inds], y[train_inds]
-    plt.plot(
-        x_train, y_train, linestyle="", marker="o", label="training data", color="r"
-    )
-    plt.plot(x, y, label="true", color="b", linestyle="--")
-    plt.fill_between(
-        x,
-        predicted_means - predicted_variances,
-        predicted_means + predicted_variances,
-        color="gray",
-        alpha=0.2,
-    )
-    plt.legend()
-    plt.xlabel("x")
-    plt.ylabel("y")
-    plt.show()
-
-
 def main(n_points: int = 300, n_frequencies: int = 3, training_set_size: int = 10):
     assert training_set_size < n_points
     x, y = generate_ground_truth_data(n_points=n_points, n_frequencies=n_frequencies)
@@ -73,6 +28,60 @@ def main(n_points: int = 300, n_frequencies: int = 3, training_set_size: int = 1
         predicted_means=predicted_means,
         predicted_variances=predicted_variances,
     )
+
+
+def radial_basis_fn(x1, x2, sigma=1.0, l=0.06):
+    """ Radial Basis Kernel """
+    return (sigma ** 2) * np.exp(-((x1 - x2) ** 2) / 2 / l ** 2)
+
+
+def generate_ground_truth_data(
+    n_points: int = 300,
+    n_frequencies: int = 3,
+    frequency_range: Tuple[float, float] = (0.5, 3.0),
+    amplitude_range: Tuple[float, float] = (0.1, 1.0),
+) -> Tuple[np.ndarray, np.ndarray]:
+    """
+    Generates 1d curve by sampling `n_frquencies` sine curves
+    :param n_points: number of points on curve
+    :param n_frequencies: Number of sine curves to superimpose
+    :param frequency_range: Range to sample sine frequencies from
+    :param amplitude_range: Range to sample sine amplitudes from
+    """
+    xmin, xmax = 0.0, 1.0
+    x = np.linspace(xmin, xmax, num=n_points)
+    y = np.zeros_like(x)
+    for _ in range(n_frequencies):
+        sampled_frequency = random.uniform(*frequency_range)
+        sampled_amplitude = random.uniform(*amplitude_range)
+        y += sampled_amplitude * np.sin(2 * np.pi * sampled_frequency * x)
+    return x, y
+
+
+def generate_plot(
+    x: np.ndarray,
+    y: np.ndarray,
+    train_inds: np.ndarray,
+    predicted_means: np.ndarray,
+    predicted_variances: np.ndarray,
+):
+    """ Plot Predictons """
+    x_train, y_train = x[train_inds], y[train_inds]
+    plt.plot(
+        x_train, y_train, linestyle="", marker="o", label="training data", color="r"
+    )
+    plt.plot(x, y, label="true", color="b", linestyle="--")
+    plt.fill_between(
+        x,
+        predicted_means - predicted_variances,
+        predicted_means + predicted_variances,
+        color="gray",
+        alpha=0.2,
+    )
+    plt.legend()
+    plt.xlabel("x")
+    plt.ylabel("y")
+    plt.show()
 
 
 if __name__ == "__main__":
